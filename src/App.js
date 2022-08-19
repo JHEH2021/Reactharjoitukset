3701c508039876d49021596c9f74c0e31072045a
JohannaEH <hyytinen.johanna@gmail.com>
2022-08-19 21:36:09 +0300

    commit kun ollut taukoa näistä...

--- src/App.js
+++ src/App.js
@@ -1,27 +1,87 @@
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-  return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
-  )
+const Statistics = (props) => {
+    if(props.all === 0) {
+        return (
+            <div>
+                No feedback given
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <table>
+                <thead>
+                    <Statistic text="Good" value ={props.good} />
+                    <Statistic text="Neutral" value ={props.neutral} />
+                    <Statistic text="Bad" value ={props.bad} />
+                    <Statistic text="All" value ={props.all} />
+                    <Statistic text="Average" value ={(props.good-props.bad) / props.all} />
+                    <Statistic text="Positive" value ={(props.good/props.all) * 100 + " %" } />
+                 </thead>
+            </table>
+        </div>
+    )
+  }
+
+const Statistic = (props) => {
+    return (
+        <tr>
+            <td>
+                {props.text}: 
+            </td>
+            <td>
+                {props.value}
+            </td>
+        </tr>
+    )
 }
 
-export default App
+
+
+
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+       {text}
+    </button>
+  )
+
+const App = (props) => {
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+    const [all, setAll] = useState(0)
+  
+
+const handleGoodClick = () => {
+    setAll(all + 1)
+    setGood(good +1) 
+  }
+
+const handleNeutralClick = () => {
+    setAll(all + 1)
+    setNeutral(neutral +1) 
+}
+const handleBadClick = () => {
+    setAll(all + 1)
+    setBad(bad +1) 
+}
+    return (
+        <div>
+            <h1>Give Feedback</h1>
+            <Button handleClick={handleGoodClick} text ='Good' />
+            <Button handleClick={handleNeutralClick} text ='Neutral' />
+            <Button handleClick={handleBadClick} text ='Bad' />
+            <h1>Statistics</h1>
+            <Statistics good={good} neutral={neutral} bad={bad} all={all} />
+        </div>
+  )
+}
+ 
+  ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+  )
+  export default App
